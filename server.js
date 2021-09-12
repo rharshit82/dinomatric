efb9ef94e893f96d0447bcfbf7bea6e3f32a0d0d
rharshit82 <rharshit82@gmail.com>
2021-09-12 14:27:17 +0530

    react build added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const socket = require('socket.io');
 const io = socket(server);
 const apiRoutes = require('./routes/apiRoutes')
 const db = require('./db/db')
+const path = require("path");
 
 require('dotenv').config()
 app.use(express.json())
@@ -13,9 +14,17 @@ app.use(express.json())
 db()
 io.on('connection', onConnection);
 
+
 function onConnection(socket){
   socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
 }
 app.use('/api',apiRoutes)
+
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 const port = 5000;
 server.listen(port, () => console.log(`server is running on port ${port}`));
