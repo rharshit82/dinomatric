6b1e4c292621de9314973972d0b1a5924ede9ba5
rharshit82 <rharshit82@gmail.com>
2021-09-12 11:06:43 +0530

    login logout added

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,17 +3,23 @@ import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
 import PersonIcon from "@material-ui/icons/Person";
 import Button from "@material-ui/core/Button";
 import { LinkContainer } from "react-router-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
 const Navbar = () => {
+  const { loginWithRedirect, isAuthenticated, logout  } = useAuth0();
   return (
     <div className="Navbar">
       <div className="containers">
         <LinkContainer to='/'><h1>DinoMatric</h1></LinkContainer>
-        <Button className="phones" color="dark">
+        <Button className="phones" color="default">
           <PhoneAndroidIcon />
         </Button>
-        <Button className="logs" color="dark">
+        {isAuthenticated?<Button className="logs" color="default" onClick={()=> logout({ returnTo: window.location.origin })}>
           <PersonIcon />
-        </Button>
+        </Button>: 
+        <Button className="logs" color="default" onClick={() => loginWithRedirect()}>
+        <PersonIcon />
+      </Button> }
+        
       </div>
     </div>
   );
