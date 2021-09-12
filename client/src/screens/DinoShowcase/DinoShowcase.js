dcf56e9e2e7b28aa3f6573a6194e2024e11e78a2
rharshit82 <rharshit82@gmail.com>
2021-09-12 10:41:32 +0530

    hello

--- client/src/screens/DinoShowcase/DinoShowcase.js
+++ client/src/screens/DinoShowcase/DinoShowcase.js
@@ -12,7 +12,7 @@ const DinoShowcase = () => {
         <div>
             <h1 className="my-5">Dino E-Museum</h1>
             <Card className="d-block mx-auto" >
-            <iframe sandbox="allow-scripts allow-same-origin allow-popups allow-forms" frameborder="0" width="600" height="600" src="https://sketchfab.com/models/516dbe9717554e74b0bfcbb77a431b17/embed"></iframe>
+            <iframe sandbox="allow-scripts allow-same-origin allow-popups allow-forms" frameborder="0" width="500" height="400" src="https://sketchfab.com/models/516dbe9717554e74b0bfcbb77a431b17/embed"></iframe>
             <Card.Body>
                 <Card.Title>Tyrannosaurus Rex</Card.Title>
                 <Card.Text>
